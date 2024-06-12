d7a81d0a44c8c794709e8d74865b65ae56739517
Navesh <navesh.professional@gmail.com>
2024-06-12 22:43:28 +0530

    Check Equality of 2 numbers

---
+++ equal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	double a,b;
+	printf("Enter first number ");
+	scanf("%lf",&a);
+	printf("Enter second number ");
+	scanf("%lf",&b);
+	if(a==b)
+	printf("EQUAL");
+	else
+	printf("NOT EQUAL");
+}
